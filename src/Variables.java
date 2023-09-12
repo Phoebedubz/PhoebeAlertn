f4e989d9c2f0505fa46531af208c4d7f2d64c5c2
100144016 <100144016@lubbockisd.net>
2023-09-12 09:24:25 -0500

    phoebeDUBZZZ

---
+++ src/Variables.java
@@ -0,0 +1,46 @@
+public class Variables
+{
+public static void main ( String[] args )
+{
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+byte byteOne = 127;
+short shortOne = -32123;
+int intOne = 90877;
+long longOne = 999999999;
+float floatOne = 38.5678F;
+double doubleOne = 923.234;
+char charOne = 'A';
+boolean booleanOne = true;
+   String stringOne = "hello world";
+System.out.println("/////////////////////////////////");
+System.out.println("*PhoebeDuBz             09/11/23*");
+System.out.println("*                               *");
+System.out.println("*        integer types          *");
+System.out.println("*                               *");
+System.out.println("*8 bit - byteOne = "+byteOne+";\t\t\t*");
+System.out.println("*16 bit - shortOne = "+shortOne+";\t*");
+System.out.println("*32 bit - intOne = "+intOne+";\t\t*");
+System.out.println("*64 bit - longOne = "+longOne+";\t*");
+    System.out.println("*\t\t\t\t\t\t\t\t*");
+    System.out.println("*         real types\t\t\t*");
+    System.out.println("*                               *");
+    System.out.println("*32 bit - floatOne = "+floatOne+";\t*");
+    System.out.println("*64 bit - doubleOne = "+doubleOne+";\t*");
+    System.out.println("*\t\t\t\t\t\t\t\t*");
+    System.out.println("*       other integer types\t\t*");
+    System.out.println("*\t\t\t\t\t\t\t\t*");
+    System.out.println("*16 bit - charOne = "+charOne+";\t\t\t*");
+    System.out.println("*\t\t\t\t\t\t\t\t*");
+    System.out.println("*           other types\t\t\t*");
+    System.out.println("*\t\t\t\t\t\t\t\t*");
+    System.out.println("*booleanOne = "+booleanOne+";\t\t\t\t*");
+    System.out.println("*stringOne = "+stringOne+";\t\t*");
+    System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+}
+}
